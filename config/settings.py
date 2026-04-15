52c2041bd723ee6cbefd4edc024b7afccbdcdf44
Deen Ali <deenali3@outlook.com>
2026-04-15 01:54:45 +0100

    Temporarily allow all hosts

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
